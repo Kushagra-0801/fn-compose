8c42b09c9efeb82d42a26a1e9126ce5417013169
Kushagra Gupta <kushgpt08@gmail.com>
2020-09-15 21:46:38 +0530

    change placeholder from ; to _

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A small macro for function composition.
 
 ```rust
 use fn_compose::fn_compose;
-let res1 = fn_compose!(1 => add_one => double => mul 3, ;, 5);
+let res1 = fn_compose!(1 => add_one => double => mul 3, _, 5);
 let res2 = mul(3, double(add_one(1)), 5);
 assert_eq!(res1, res2);
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@
 macro_rules! fn_compose {
     ( $arg: expr ) => { $arg };
     ( $arg: expr => $f: ident ) => { $f($arg) };
-    ( $arg: expr => $f: ident $( $largs: expr, )* ; $( ,$rargs: expr )* ) => {
+    ( $arg: expr => $f: ident $( $largs: expr, )* _ $( ,$rargs: expr )* ) => {
         $f($($largs,)* $arg $(,$rargs)*)
     };
     ( $arg: expr => $f: ident => $( $tokens: tt )* ) => {{
         fn_compose!($f($arg) => $($tokens)*)
     }};
-    ( $arg: expr => $f: ident $( $largs: expr, )* ; $( ,$rargs: expr )* => $( $tokens: tt )* ) => {{
+    ( $arg: expr => $f: ident $( $largs: expr, )* _ $( ,$rargs: expr )* => $( $tokens: tt )* ) => {{
         fn_compose!($f($($largs,)* $arg $(,$rargs)*) => $($tokens)*)
     }};
 }
@@ -33,7 +33,7 @@ mod tests {
     #[test]
     fn one_arg_fn() {
         let add = |x, y| x + y;
-        assert_eq!(fn_compose!(4 => add 5, ;), 9);
+        assert_eq!(fn_compose!(4 => add 5, _), 9);
     }
 
     #[test]
@@ -47,6 +47,6 @@ mod tests {
     fn two_arg_fn() {
         let mul = |a, b, c, d, e| a * b * c * d * e;
         let add = |a, b, c| a + b + c;
-        assert_eq!(fn_compose!(4 => add 3, ;, 9 => mul 1, 4, 2, ;, 10), 1280);
+        assert_eq!(fn_compose!(4 => add 3, _, 9 => mul 1, 4, 2, _, 10), 1280);
     }
 }
